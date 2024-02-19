6f63cf2f4c5bdd97e0363333cc03a4b86c338ec8
Šefčík Lukáš <sefcik@kros.sk>
2024-02-19 13:53:36 +0100

    first commit

---
+++ src/wwElement.vue
@@ -0,0 +1,81 @@
+<template>
+  <DxDraggable id="list"
+               :group="'testing'"
+               :on-drag-start="onListDragStart"
+               data="dropArea">
+    <DxDraggable v-for="(item, index) in content.data"
+                 :key="index"
+                 :clone="true"
+                 :group="'testing'"
+                 :data="item"
+                 :on-drag-start="onItemDragStart"
+                 :on-drag-end="onItemDragEnd"
+                 class="item dx-card dx-theme-text-color dx-theme-background-color">
+      <wwLayoutItemContext :index="index"
+                           :item="null"
+                           is-repeat
+                           :data="item"
+                           :repeated-items="content.data">
+        <wwElement v-bind="content.itemContainer"></wwElement>
+      </wwLayoutItemContext>
+    </DxDraggable>
+  </DxDraggable>
+</template>
+
+<script>
+import './dx.fluent.dx-light-theme.css';
+
+import DxDraggable from 'devextreme-vue/draggable';
+
+export default {
+  components: {
+    DxDraggable
+  },
+  props: {
+    content: {type: Object, required: true}
+  },
+  emits: ['trigger-event'],
+  computed: {},
+  methods: {
+    onListDragStart(event) {
+      console.log('onListDragStart', event);
+      event.cancel = true;
+    },
+    onItemDragStart(event) {
+      console.log('onItemDragStart', event);
+      event.itemData = event.fromData;
+    },
+    onItemDragEnd(event) {
+      console.log('onItemDragEnd', event);
+      if (event.toData) {
+        event.cancel = true;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+#scroll,
+#list {
+  position: relative;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  width: 240px;
+}
+
+.item {
+  box-sizing: border-box;
+  padding: 10px 20px;
+  margin-bottom: 10px;
+}
+
+.dx-draggable-source {
+  opacity: 0.5;
+}
+
+.dx-draggable-dragging > * {
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 6px 8px rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ ww-config.js
@@ -0,0 +1,49 @@
+export default {
+    editor: {
+        label: {
+            en: "kros-draggable",
+        }
+    },
+    triggerEvents: [
+        { name: 'mousedown', label: { en: 'broMousedown' }, event: { value: [] }, default: true }
+    ],
+    inherit: {
+        type: "ww-layout"
+    },
+    options: {
+        layout: ["flex"]
+    },
+    properties: {
+        data: {
+            label: {
+                en: "📦 Data"
+            },
+            type: "Info",
+            options: {
+                text: {
+                    en: "Bind your data"
+                }
+            },
+            bindable: "repeatable",
+            section: "settings",
+            bindingValidation: {
+                validations: [
+                    {
+                        type: "array"
+                    },
+                    {
+                        type: "object"
+                    }
+                ],
+                tooltip: "An array of data ``[{...}, {...}, {...}]``, <br> or a collection."
+            }
+        },
+        itemContainer: {
+            hidden: true,
+            defaultValue: {
+                isWwObject: true,
+                type: "ww-flexbox"
+            }
+        }
+    },
+};
